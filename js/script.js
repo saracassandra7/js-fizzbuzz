09d756c9928dcb7c2ad30f4180419f3d723d52a9
saracassandra7 <saracassandra7@gmail.com>
2022-10-13 17:19:05 +0200

    [add] different styles in boxes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 4) per i multipli di 3 scrivo Fizz ---> fatto
 
 5) creo container in DOM in cui metto i box con i numeri ---> fatto
-6) metto colori differenti ai box in base a quello che c'è scritto
+6) metto colori differenti ai box in base a quello che c'è scritto --->fatto
 */ 
 
 const fizz = 3;
@@ -15,6 +15,7 @@ const fizzBuzz = 15;
 const container =document.querySelector('.container');
 const limit = 100;
 
+//console
 for(let i = 1; i <= 100; i++){
 
   if(i % fizzBuzz === 0){
@@ -32,6 +33,7 @@ for(let i = 1; i <= 100; i++){
 
 }
 
+//output in pagina
 for(let i = 1; i<=limit; i++){
   //creo il box
   const box = document.createElement('div');
@@ -44,4 +46,22 @@ for(let i = 1; i<=limit; i++){
 
   //aggiungo il box al container
   container.append(box);
+
+  //cambio colore in base a cosa c'è scritto
+  if(i % fizzBuzz === 0){
+    box.classList.add('fizzbuzz');
+    box.innerHTML = "Fizz <br> Buzz";
+
+  }else if (i % buzz ===0){
+    box.classList.add('buzz');
+    box.innerHTML = "Buzz";
+
+  }else if (i % fizz === 0){
+    box.classList.add('fizz');
+    box.innerHTML = "Fizz";
+
+  }else{
+    box.classList.add('default');
+  }
+
 }
