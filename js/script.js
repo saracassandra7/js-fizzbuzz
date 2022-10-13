e76f15f16cecf9e69d8a5cab998e3f2825a8810d
saracassandra7 <saracassandra7@gmail.com>
2022-10-13 16:37:05 +0200

    [end] exercise in console

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /*
 1) stampo in console i numeri da 1 a 100 ---> fatto
-2) per i multipli di 3 scrivo Fizz
-3) per i multipli di 5 scrivo Buzz
-4) per i multipli sia di 3 che di 5 scrivo FizzBuzz
+2) per i multipli sia di 3 che di 5 scrivo FizzBuzz ---> fatto
+3) per i multipli di 5 scrivo Buzz ---> fatto
+4) per i multipli di 3 scrivo Fizz ---> fatto
 */ 
 
 const fizz = 3;
@@ -11,18 +11,17 @@ const fizzBuzz = 15;
 
 for(let i = 1; i <= 100; i++){
 
-  if(i % fizz === 0){
-    console.log(i, 'Fizz');
+  if(i % fizzBuzz === 0){
+    console.log(i, 'FizzBuzz');
 
   }else if (i % buzz ===0){
     console.log(i, 'Buzz');
 
-   /*}else if (i % fizz ===0 && i % buzz === 0){
-    console.log(i, 'FizzBuzz');
-   }*/
-  
-  }else if (i % fizzBuzz === 0){
-    console.log(i, 'FizzBuzz');
+  }else if (i % fizz === 0){
+    console.log(i, 'Fizz');
+
+  }else{
+    console.log(i);
   }
 
 }
